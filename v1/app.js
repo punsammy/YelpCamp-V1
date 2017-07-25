ba310fdce8611b8b6507cf9f7b86436c07a450eb
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-07-25 17:03:45 -0400

    add form to add campground

--- v1/app.js
+++ v1/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -16,6 +18,14 @@ app.get('/campgrounds', function(req, res) {
   ]
 
   res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res) {
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res) {
+  res.render('new.ejs');
 });
 
 app.listen(process.env.PORT || 3000, function(){
